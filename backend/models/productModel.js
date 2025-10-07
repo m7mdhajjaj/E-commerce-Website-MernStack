145ff1add5767d8bc6baedf1196cd36a886f319c
mohammad hajjaj <mohd.hajjaj80@gmail.com>
2025-10-07 23:02:03 +0300

    ..

---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array, required: true },
+  bestSeller: { type: Boolean, required: true },
+  date: { type: Number, required: true },
+});
+
+const Product = mongoose.model("Product", productSchema) || mongoose.models.Product;
+
+export default Product;
